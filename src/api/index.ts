24d70f06a890f2900d4f93beaf11209a1275b486
FilimonovKostya <konstantinfilimonou@gmail.com>
2021-12-18 13:28:19 +0300

    fix api request

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,23 +1,12 @@
 import axios from "axios";
 import {ISearchResponse} from "./types";
 
-// const url = 'api.openweathermap.org/data/2.5/weather?Q=London,uk&APPID=3cd1cd1507089288f25c162d3747cbae'
-
 const instance = axios.create({
-    baseURL: 'http://api.openweathermap.org/data/2.5/weather?q=Minsk&appid=3cd1cd1507089288f25c162d3747cbae',
+    baseURL: 'http://api.openweathermap.org/data/2.5/weather',
+    params: {appid: '3cd1cd1507089288f25c162d3747cbae'}
 })
 
 
-const searchAPI = (value: string) => {
-    return instance.get(``)
-}
+const searchAPI = (value: string) => instance.get<ISearchResponse>(`?q=${value}`)
 
 export default searchAPI
-
-
-// const url = 'api.openweathermap.org/data/2.5/weather?q=Minsk'
-//
-// const instance = axios.create({
-//     baseURL: url,
-//     params: {appid:'3cd1cd1507089288f25c162d3747cbae'}
-// })
--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,20 +1,8 @@
-import React, {useEffect} from 'react';
+import React from 'react';
 import Search from 'components/Search';
-import searchAPI from "../api";
 
 
 function App() {
-
-    useEffect(() => {
-        searchAPI('Minsk')
-            .then(res => console.log('res', res))
-            .catch(err => console.log('error', err))
-
-        // fetch('api.openweathermap.org/data/2.5/weather?appid=3cd1cd1507089288f25c162d3747cbae&q=Minsk')
-        //     .then(res => console.log('response', res))
-
-    }, [])
-
 
     return <div className={'app'}>
         <Search/>
