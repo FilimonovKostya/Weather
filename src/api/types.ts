9954116e77e4160837cfe335ea0ae26c0385b87c
FilimonovKostya <konstantinfilimonou@gmail.com>
2021-12-29 10:50:09 +0300

    was added types for forecast days

--- src/api/types.ts
+++ src/api/types.ts
@@ -1,51 +1,80 @@
 export interface IWeatherCardResponse {
     coord: {
-        lon: number,
+        lon: number
         lat: number
     },
     weather: [{
-        id: number,
-        main: string,
-        description: string,
+        id: number
+        main: string
+        description: string
         icon: string
     }],
-    base: string,
+    base: string
     main: {
-        temp: number,
-        feels_like: number,
-        temp_min: number,
-        temp_max: number,
-        pressure: number,
-        humidity: number,
-        sea_level: number,
+        temp: number
+        feels_like: number
+        temp_min: number
+        temp_max: number
+        pressure: number
+        humidity: number
+        sea_level: number
         grnd_level: number
     },
-    visibility: number,
+    visibility: number
     wind: {
-        speed: number,
-        deg: number,
-        gust: number,
+        speed: number
+        deg: number
+        gust: number
     },
     clouds: {
         all: number
     },
-    dt: number,
+    dt: number
     sys: {
-        type: number,
-        id: number,
-        country: string,
-        sunrise: number,
+        type: number
+        id: number
+        country: string
+        sunrise: number
         sunset: number
     },
-    timezone: number,
-    id: number,
-    name: string,
+    timezone: number
+    id: number
+    name: string
     cod: number
 }
 
+type DailiesWeather = {
+    id: number
+    main: string
+    description: string
+    icon: string
+}
+
+type Dailies = {
+    clouds: number
+    dew_point: number
+    dt: number
+    feels_like: { day: number, night: number, eve: number, morn: number }
+    humidity: number
+    moon_phase: number
+    moonrise: number
+    moonset: number
+    pop: number
+    pressure: number
+    sunrise: number
+    sunset: number
+    temp: { day: number, min: number, max: number, night: number, eve: number, morn: number }
+    uvi: number
+    weather: DailiesWeather[]
+    wind_deg: number
+    wind_gust: number
+    wind_speed: number
+}
+
 export interface IForecastDaysResponse {
-    lat: number,
-    lon: number,
+    daily: Dailies[]
+    lat: number
+    lon: number
     timezone: string,
     timezone_offset: number
 }
