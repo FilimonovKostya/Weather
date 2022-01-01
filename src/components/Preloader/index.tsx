4bd649fa359795090b6de309898ef601738c91ff
FilimonovKostya <konstantinfilimonou@gmail.com>
2022-01-01 18:41:47 +0300

    added error message for preloader

--- src/components/Preloader/index.tsx
+++ src/components/Preloader/index.tsx
@@ -1,15 +1,21 @@
 import React from 'react';
 import style from './style.module.scss'
+import {useSelector} from "react-redux";
+import {RootState} from "store/store";
 
 
-const Preloader = () => (
-    <div className={style.wrapper}>
+const Preloader = () => {
+
+    const errorMessage = useSelector<RootState, string>(state => state.status.errorMessage)
+
+    return <div className={style.wrapper}>
         <div className={style.container}>
             <div className={style.ring}/>
             <div className={style.ring}/>
             <div className={style.ring}/>
         </div>
+        {errorMessage && <span> {errorMessage} </span>}
     </div>
-)
+}
 
 export default Preloader;
